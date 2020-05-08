a0a54276026e6dea98aacbbee73c735d7f9898c8
shizuilab <shizuinet@gmail.com>
2020-05-09 07:40:32 +0900

    new files

---
+++ shutdown_by_button.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import os, time, commands, subprocess
+from pijuice import PiJuice
+#import Adafruit_SSD1306
+
+#import Image
+#import ImageDraw
+#import ImageFont
+
+GPIO.setmode(GPIO.BCM)
+
+pj = PiJuice(1, 0x14)
+
+# Raspberry Pi pin configuration
+#RST = 24
+#disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Initialize library.
+#disp.begin()
+
+# Clear display.
+#disp.clear()
+#disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+#width = disp.width
+#height = disp.height
+#image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+#draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+#draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+#font = ImageFont.truetype('/usr/share/fonts/truetype/kochi/kochi-gothic-subst.ttf', 16, encoding='unic')
+
+# GPIO6 : shutdown button = second raw of AIY shield
+GPIO.setup(6, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+while True:
+    GPIO.wait_for_edge(6, GPIO.FALLING)
+    sw_counter = 0
+ 
+    while True:
+        sw_status = GPIO.input(6)
+        if sw_status == 0:
+            sw_counter = sw_counter + 1
+            if sw_counter >= 5:
+
+            # Shutdown Message
+                pj.power.SetPowerOff(30)
+                #os.system("sudo systemctl stop meter.service")
+                time.sleep(1)
+
+                #draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+                #mystring= u'VT250F'
+                #draw.text((0,0), mystring, font=font, fill=255)
+                #mystring= u'内部バッテリ'
+                #draw.text((0,20), mystring, font=font, fill=255)
+                #mystring= u'に接続します'
+                #draw.text((0,40), mystring, font=font, fill=255)
+
+                #disp.image(image)
+                #disp.display()
+                #time.sleep(1)
+
+                os.system("sudo /home/pi/shutdownVT.sh")
+                #os.system("sudo shutdown -h now")
+                break
+        else:
+            print("signal not significant")
+            break
+
+        time.sleep(0.01)
+     
+    print(sw_counter)
---
+++ youtube-c270.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+sleep 5
+
+sudo /bin/bash /home/pi/usbreset.sh
+
+if [ ! -e /dev/video0 ]; then
+/home/pi/aquestalkpi/AquesTalkPi "ビデオカメラが見つかりません" | aplay
+exit 0
+fi
+
+#if [ ! -d /dev/snd/by-id ]; then
+#/home/pi/aquestalkpi/AquesTalkPi "マイクが見つかりません" | aplay
+#exit 0
+#fi
+
+#if [ ! -d /media/pi/3DF7-5138 ]; then
+#/home/pi/aquestalkpi/AquesTalkPi "USBメモリがみつかりません" | aplay
+#exit 0
+#fi
+
+if [ -f /var/tmp/location.txt ]; then
+ /home/pi/aquestalkpi/AquesTalkPi "ユーチューブライブストリーミングを開始します" | aplay
+
+ ffmpeg\
+ -f alsa -thread_queue_size 8192 -i plughw:2,0\
+ -f v4l2 -thread_queue_size 8192 -input_format yuyv422 -video_size 640x360 -framerate 30 -i /dev/video0\
+ -vf drawtext="fontfile=/usr/share/fonts/truetype/fonts-japanese-gothic.ttf:\
+ fontcolor=#FFFFFF:fontsize=20:x=10:y=10:box=1:boxcolor=black@0.4:\
+ textfile='/var/tmp/location.txt':reload=1":\
+ -c:v h264_omx -b:v 1000k -bufsize 1500k -vsync 0\
+ -c:a aac -ab 128k -af volume=0.9 -g 16 -t 600\
+ -f flv rtmp://a.rtmp.youtube.com/live2/u3wc-1gyz-71c7-6waj
+
+else
+ /home/pi/aquestalkpi/AquesTalkPi "ロケーションファイルが見つかりません" | aplay
+ #ffmpeg -thread_queue_size 8192\
+ #-f concat -safe 0 -i /media/pi/USB0/BGM/mylist.txt\
+ #-f v4l2 -input_format yuyv422 -video_size 640x360 -framerate 30 -i /dev/video0\
+ #-vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:\
+ #fontcolor=#FFFFFF:fontsize=30:x=10:y=10:box=1:boxcolor=black@0.4:\
+ #text='%{localtime}'"\
+ #-c:v h264_omx -b:v 1000k -bufsize 1500k -vsync 0\
+ #-c:a aac -ab 128k -af volume=0dB -g 16 -t 600\
+ #-f flv rtmp://a.rtmp.youtube.com/live2/esgj-4ht8-093r-7484
+fi
