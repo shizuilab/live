c03f63ce2b447d44d740123a4993272ba4110d3d
shizuilab <shizuinet@gmail.com>
2020-05-10 21:55:27 +0900

    minor changes

--- youtube-c270.sh
+++ youtube-c270.sh
@@ -6,22 +6,15 @@ sleep 5
 
 sudo /bin/bash /home/pi/usbreset.sh
 
+cat /media/pi/3DF7-5138/BGM/mylist.txt | sort -R > /home/pi/playlist.txt
+
 if [ ! -e /dev/video0 ]; then
-/home/pi/aquestalkpi/AquesTalkPi "ビデオカメラが見つかりません" | aplay
-exit 0
+ /home/pi/aquestalkpi/AquesTalkPi "ビデオカメラが見つかりません" | aplay
+ exit 0
 fi
 
-#if [ ! -d /dev/snd/by-id ]; then
-#/home/pi/aquestalkpi/AquesTalkPi "マイクが見つかりません" | aplay
-#exit 0
-#fi
 
-#if [ ! -d /media/pi/3DF7-5138 ]; then
-#/home/pi/aquestalkpi/AquesTalkPi "USBメモリがみつかりません" | aplay
-#exit 0
-#fi
-
-if [ -f /var/tmp/location.txt ]; then
+if [[ $(arecord -l) == *'USB Audio Device'* ]]; then
  /home/pi/aquestalkpi/AquesTalkPi "ユーチューブライブストリーミングを開始します" | aplay
 
  ffmpeg\
@@ -31,18 +24,19 @@ if [ -f /var/tmp/location.txt ]; then
  fontcolor=#FFFFFF:fontsize=20:x=10:y=10:box=1:boxcolor=black@0.4:\
  textfile='/var/tmp/location.txt':reload=1":\
  -c:v h264_omx -b:v 1000k -bufsize 1500k -vsync 0\
- -c:a aac -ab 128k -af volume=0.9 -g 16 -t 600\
- -f flv rtmp://a.rtmp.youtube.com/live2/u3wc-1gyz-71c7-6waj
+ -c:a aac -ab 128k -af volume=0.9 -g 16 -t 3600\
+ -f flv rtmp://a.rtmp.youtube.com/live2/ju63-rzf3-te44-c373
 
 else
- /home/pi/aquestalkpi/AquesTalkPi "ロケーションファイルが見つかりません" | aplay
- #ffmpeg -thread_queue_size 8192\
- #-f concat -safe 0 -i /media/pi/USB0/BGM/mylist.txt\
- #-f v4l2 -input_format yuyv422 -video_size 640x360 -framerate 30 -i /dev/video0\
- #-vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSerif.ttf:\
- #fontcolor=#FFFFFF:fontsize=30:x=10:y=10:box=1:boxcolor=black@0.4:\
- #text='%{localtime}'"\
- #-c:v h264_omx -b:v 1000k -bufsize 1500k -vsync 0\
- #-c:a aac -ab 128k -af volume=0dB -g 16 -t 600\
- #-f flv rtmp://a.rtmp.youtube.com/live2/esgj-4ht8-093r-7484
+ /home/pi/aquestalkpi/AquesTalkPi "BGM付きストリーミングを開始します" | aplay
+
+ ffmpeg -thread_queue_size 8192\
+ -f concat -safe 0 -i /home/pi/playlist.txt\
+ -f v4l2 -input_format yuyv422 -video_size 640x360 -framerate 30 -i /dev/video0\
+ -vf drawtext="fontfile=/usr/share/fonts/truetype/fonts-japanese-gothic.ttf:\
+ fontcolor=#FFFFFF:fontsize=20:x=10:y=10:box=1:boxcolor=black@0.4:\
+ textfile='/var/tmp/location.txt':reload=1":\
+ -c:v h264_omx -b:v 1000k -bufsize 1500k -vsync 0\
+ -c:a aac -ab 128k -af volume=-10dB -g 16 -t 3600\
+ -f flv rtmp://a.rtmp.youtube.com/live2/ju63-rzf3-te44-c373
 fi
