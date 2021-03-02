c69259b5c29c5faa72a98f2ba2267c7a490d1cbf
shizuilab <shizuilab@gmail.com>
2021-03-02 14:02:07 +0900

    update for pi4 migration

---
+++ config/live_config_sample.js
@@ -0,0 +1,11 @@
+// sample.jsをwanapi-nis1.jsにリネームしてご使用ください
+
+module.exports = {
+    AUTH: "2cb6c3b748b84f34a0f7fb5cae447ded", //Blynkのキー
+    PRIVATEKEY: "58e386e36834baf503b0111256585b1eedcb2bab6526078d0049019c9abb8d00", //送り元の秘密鍵
+    ADDRESS: "NDQM3DXHCKRT7N6223QHNK655PZL6IJZQEIHHR6Q", //送り先のNEMアドレス
+    NAMESPACE: "shizui", //ネームスペース
+    MOSAIC: "wanapi", //モザイク名（nemlog記事にリクエストいただければお分けします)
+    PLACE: "locator" //WanaPi!を設置する場所（プレフィックス）
+}
+
