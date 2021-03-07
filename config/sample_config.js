c7a8354f34ef0699502336c49a2376964cd5c3bd
shizuilab <shizuilab@gmail.com>
2021-03-07 09:57:30 +0900

    created config sample

--- config/sample_config.js
+++ config/sample_config.js
@@ -1,9 +1,9 @@
-// sample.jsをwanapi-nis1.jsにリネームしてご使用ください
+// sample_config.jsをlive_config.jsにリネームしてご使用ください
 
 module.exports = {
-    AUTH: "", //Blynkのキー
-    PRIVATEKEY: "", //送り元の秘密鍵
-    ADDRESS: "NDQM3DXHCKRT7N6223QHNK655PZL6IJZQEIHHR6Q", //送り先のNEMアドレス
+    AUTH: "***********", //Blynkのキー
+    PRIVATEKEY: "*************", //送り元の秘密鍵
+    ADDRESS: "N************", //送り先のNEMアドレス
     NAMESPACE: "shizui", //ネームスペース
     MOSAIC: "wanapi", //モザイク名（nemlog記事にリクエストいただければお分けします)
     PLACE: "locator" //WanaPi!を設置する場所（プレフィックス）
